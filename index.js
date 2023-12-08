7d28ea739c18830ad4b2ebe82d76080758367416
ArbabAhmed1988 <153269617+ArbabAhmed1988@users.noreply.github.com>
2023-12-08 23:36:55 +0500

    Update index.js

--- index.js
+++ index.js
@@ -10,13 +10,18 @@ const postRoute = require("./routes/posts");
 
 dotenv.config();
 
-mongoose.connect(
-  process.env.MONGO_URL,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  () => {
-    console.log("Connected to MongoDB");
-  }
-);
+const Connect =() =>{
+  console.log('====================================', process.env.MONGO_URL);
+
+  mongoose.connect(process.env.MONGO_URL)
+  .then(()=>{
+      console.log("Database Connected")
+  })
+  .catch((err)=>{
+      throw err;
+  })
+
+}
 
 //middleware
 app.use(express.json({ limit: "50mb" }));
@@ -29,35 +34,6 @@ app.use("/api/posts", postRoute);
 
 app.listen(8800, () => {
   console.log("Backend server is running! ");
+  Connect();
 });
 
-javascript: (function () {
-  let accept = document.querySelectorAll(".iubenda-cs-btn-primary");
-  let allDiv = document.querySelectorAll(".list");
-  let num1 = Math.round(Math.random() * 1);
-  console.log(num1);
-  let click2 = allDiv[0].children[num1].children[0].getAttribute("href");
-  let second;
-  let newTab;
-  let num;
-  do {
-    num = Math.floor(Math.random() * 40001) + 60000;
-    console.log(num);
-  } while (num < 60000 || num > 100000);
-  setTimeout(() => {
-    accept[0].click();
-    second();
-  }, 4000);
-  second = setTimeout(() => {
-    newTab = window.open(click2);
-    setTimeout(() => {
-      console.log("Closing the tab...");
-      newTab.close();
-      console.log("Closing the main...");
-      setTimeout(() => {
-        console.log("Closing the main...");
-        window.close();
-      }, 2000);
-    }, num);
-  }, 10000);
-})();
